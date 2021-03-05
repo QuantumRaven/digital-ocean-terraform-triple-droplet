a020c66808065f5f893d11c535f745451f1b1c45
QuantumRaven <quantum-raven@outlook.com>
2021-03-04 22:42:51 -0500

    Removed uncessary text

--- locals.tf
+++ locals.tf
@@ -1,11 +1,9 @@
 locals {
 
     whitelist_ips = [
-<<<<<<< HEAD
+
         "your_public_ip_here/32"
-=======
-        "your_ip_here/32"
->>>>>>> d89eefe3f0f95a46e12fe578736314903ba5420c
+
     ]
 
     droplets = [
